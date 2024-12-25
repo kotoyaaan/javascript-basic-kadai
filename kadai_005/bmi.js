dec4de62732c431d437217a5290691e1e8373974
kotoyaaan <narumimurakami0115@gmail.com>
2024-12-25 23:57:18 +0900

    課題のファイルをアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+// 体重(kg)と身長(m)の変数を宣言・値の代入
+let userWeight = 68;
+let userHight = 1.7;
+
+// bmiの変数を宣言、計算結果の代入
+let bmi = (userWeight / (userHight * userHight));
+
+// bmi計算式の結果の出力
+console.log(bmi);
